e501ec47591c3afee84d2f51b2f7ea77e6af29bc
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-11-23 23:21:48 +0900

    Title: 격자 반대로 채우기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241123/격자 반대로 채우기/grid-anti-fill.py
+++ 241123/격자 반대로 채우기/grid-anti-fill.py
@@ -8,15 +8,28 @@ for i in range(n):
 
 count = 0
 
-for i in range(n-1, -1, -1):
-    if i % 2 == 1:
-        for j in range(n-1, -1, -1):
-            count = count + 1
-            arr[j][i] = count
-    else:
-        for j in range(n):
-            count = count + 1
-            arr[j][i] = count
+if n % 2 == 0:
+    for i in range(n-1, -1, -1):
+        if i % 2 == 1:
+            for j in range(n-1, -1, -1):
+                count = count + 1
+                arr[j][i] = count
+        else:
+            for j in range(n):
+                count = count + 1
+                arr[j][i] = count
+else:
+    for i in range(n-1, -1, -1):
+        if i % 2 == 0:
+            for j in range(n-1, -1, -1):
+                count = count + 1
+                arr[j][i] = count
+        else:
+            for j in range(n):
+                count = count + 1
+                arr[j][i] = count
+
+
 
 for k in range(n):
     for h in range(n):
