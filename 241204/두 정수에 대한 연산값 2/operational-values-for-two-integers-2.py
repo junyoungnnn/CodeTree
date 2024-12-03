122d17ac5db3d7079ccbc48f0dc49783aed65ef6
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-12-04 01:08:27 +0900

    Title: 두 정수에 대한 연산값 2, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241204/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 241204/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,9 +1,12 @@
 a, b = map(int, input().split())
 
 def cel(a, b):
-    if a > b : a, b = b, a
-    a += 10
-    b *= 2
+    if a > b :
+        b += 10
+        a *= 2
+    else:
+        a += 10
+        b *= 2
     return a, b
 
 A, B = cel(a, b)
