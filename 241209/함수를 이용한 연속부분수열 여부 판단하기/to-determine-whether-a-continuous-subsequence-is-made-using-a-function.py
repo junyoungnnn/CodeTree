8bf03333bf8acaada9ab7169b63342f17d717f9f
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-12-09 00:17:27 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 67ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241209/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 241209/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,4 +1,4 @@
-def cal(n1, n2, a, b):
+def cal():
     for j in range(n1):
         if b[0] == a[j]:
             if b[n2 - 1] == a[j + n2 - 1]:
@@ -10,7 +10,7 @@ n1, n2 = map(int, input().split())
 a = input().split()
 b = input().split()
 
-if cal(n1, n2, a, b):
+if cal():
     print('Yes')
 else:
     print('No')
