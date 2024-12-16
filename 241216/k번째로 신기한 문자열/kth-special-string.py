ccf1c2f857d212fc1ca2f62e10413a7231bed9a2
Jun-Young Noh <njy3006@gmail.com>
2024-12-16 21:51:01 +0900

    Title: k번째로 신기한 문자열, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241216/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,14 @@
+n, k, T = input().split()
+
+n, k = int(n), int(k)
+
+arr = []
+
+for i in range(n):
+    data = input()
+    if data.find(T) != -1:
+        arr.append(data)
+
+arr = sorted(arr)
+
+print(arr[k-1])
