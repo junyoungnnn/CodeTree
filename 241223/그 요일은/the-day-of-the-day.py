0e5d13af60bb82e1cb1b57bc65296a637a45f27d
Jun-Young Noh <njy3006@gmail.com>
2024-12-23 02:31:54 +0900

    Title: 그 요일은, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/그 요일은/the-day-of-the-day.py
+++ 241223/그 요일은/the-day-of-the-day.py
@@ -9,13 +9,8 @@ day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 elapsed_days1 = 0
 elapsed_days2 = 0
 
-for i in range(1, m1):
-    elapsed_days1 += month[i]
-elapsed_days1 += d1 -1
-
-for i in range(1, m2):
-    elapsed_days2 += month[i]
-elapsed_days2 += d2 -1
+elapsed_days1 = sum(month[:m1]) + d1 - 1
+elapsed_days2 = sum(month[:m2]) + d2 - 1
 
 start_day_idx = 0
 day1_idx = (start_day_idx + elapsed_days1) % 7
