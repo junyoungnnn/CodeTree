b6b5a5089531d27aa78f38d126ef6e57fbfbe1ab
Jun-Young Noh <njy3006@gmail.com>
2024-12-23 01:20:08 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241223/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,10 @@
+a, b, c = map(int, input().split())
+
+total_min = (a- 11) * 24 * 60 + b * 60 + c
+
+std_min = 11 * 60 + 11
+
+if total_min - std_min > 0:
+    print(total_min - std_min)
+else:
+    print(-1)
