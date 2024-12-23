181e8e18d195d0c3696166f4f9888dcbf429dd8a
Jun-Young Noh <njy3006@gmail.com>
2024-12-23 14:26:56 +0900

    Title: 그 요일은, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/그 요일은/the-day-of-the-day.py
+++ 241223/그 요일은/the-day-of-the-day.py
@@ -9,19 +9,17 @@ day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 elapsed_days1 = 0
 elapsed_days2 = 0
 
-elapsed_days1 = sum(month[:m1]) + d1 - 1
-elapsed_days2 = sum(month[:m2]) + d2 - 1
+for i in range(1, m1):
+    elapsed_days1 += month[i]
+elapsed_days1 += d1 -1
 
-start_day_idx = 0
-day1_idx = (start_day_idx + elapsed_days1) % 7
-day2_idx = (start_day_idx + elapsed_days2) % 7
-
-target_day_idx = day.index(input_day)
+for i in range(1, m2):
+    elapsed_days2 += month[i]
+elapsed_days2 += d2 -1
 
 count = 0
-for days in range(elapsed_days1, elapsed_days2 + 1):
-    current_day_idx = (start_day_idx + days) % 7
-    if current_day_idx == target_day_idx:
+for i in range(elapsed_days1, elapsed_days2 + 1):
+    if i % 7 == day.index(input_day):
         count += 1
 
 print(count)
