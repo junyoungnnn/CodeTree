91a7edb2faeaeda071c0412a8e7ed7fe5c273c53
Jun-Young Noh <njy3006@gmail.com>
2024-12-25 01:17:40 +0900

    Title: 최대로 겹치는 구간, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 241225/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 241225/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -4,7 +4,7 @@ arr = [0] * 201
 
 for i in range(n):
     a, b = map(int, input().split())
-    for i in range(a+100, b+101):
+    for i in range(a+101, b+101):
         arr[i] += 1
 
 print(max(arr))
