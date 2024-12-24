fe77d49e5c7ea31e937f53b8a1454b9770ba3cb0
Jun-Young Noh <njy3006@gmail.com>
2024-12-25 02:03:15 +0900

    Title: 왔다 갔던 구역 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241225/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241225/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -3,25 +3,38 @@ n = int(input())
 arr = [0] * 201
 start = 0
 
+T2 = ''
 for i in range(n):
-    x, T = input().split()
+    x, T1 = input().split()
     x = int(x)
-    if T == 'R':
-        for j in range(start, start + x + 1):
-            arr[j] += 1
-        start = start + x
+    if T1 == T2:
+        if T1 == 'R':
+            for j in range(start+1, start + x + 1):
+                arr[j] += 1
+            start = start + x
+        else:
+            for j in range(start-1, start - x - 1, -1):
+                arr[j] += 1
+            start = start - x
     else:
-        for j in range(start, start - x - 1, -1):
-            arr[j] += 1
-        start = start - x
-        
+        if T1 == 'R':
+            for j in range(start, start + x + 1):
+                arr[j] += 1
+            start = start + x
+        else:
+            for j in range(start, start - x - 1, -1):
+                arr[j] += 1
+            start = start - x
+    T2 = T1
+
 count = 0
 c = 0
+
 for i in arr:
     if i >= 2:
         c += 1
     else:
-        if c != 0:
+        if c > 0:
             c -= 1
             count += c
             c = 0
