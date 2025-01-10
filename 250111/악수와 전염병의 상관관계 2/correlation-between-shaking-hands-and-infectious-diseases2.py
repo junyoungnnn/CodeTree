0e66c724dd0963ba6899e208eca4ad785b47c1f9
Jun-Young Noh <njy3006@gmail.com>
2025-01-11 02:44:18 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250111/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250111/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -9,10 +9,14 @@ handshakes.sort()
 result[P-1] = 1
 
 for t, x, y in handshakes:
-    if (count[x-1] > 0 and result[x-1] == 1) or (count[y-1] > 0 and result[y-1] == 1):
-        result[x-1] = 1
+    if result[x-1] == 1 and result[y-1] == 1:
+        count[x-1] -= 1
+        count[y-1] -= 1
+    elif (count[x-1] > 0 and result[x-1] == 1) and result[y-1] == 0:
         result[y-1] = 1
         count[x-1] -= 1
+    elif (count[y-1] > 0 and result[y-1] == 1) and result[x-1] == 0:
+        result[x-1] = 1
         count[y-1] -= 1
 
 for i in result:
