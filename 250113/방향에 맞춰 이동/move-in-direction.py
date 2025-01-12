cf3c1bba4f1a3301e02ebfa4399374dbb6e93dc6
Jun-Young Noh <njy3006@gmail.com>
2025-01-13 02:03:38 +0900

    Title: 방향에 맞춰 이동, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250113/방향에 맞춰 이동/move-in-direction.py
+++ 250113/방향에 맞춰 이동/move-in-direction.py
@@ -9,32 +9,10 @@ for i in range(n):
     distance = int(distance)
     if direction == 'E':
         nx, ny = dx[0] * distance, dy[0]
-    
-
-    x += nx
-    y += ny
-
-print(x, y)
-
-테스트케이스
-
-
-결과
-
-
-코드 실행
-
-
-제출 및 채점
-
-
-
     elif direction == 'S':
         nx, ny = dx[1], dy[1] * distance
-    
     elif direction == 'W':
         nx, ny = dx[2] * distance, dy[2]
-
     elif direction == 'N':
         nx, ny = dx[3] , dy[3] * distance
 
