44b833e16b97767f26f9cc2b9367e8f8da5b1bb0
Jun-Young Noh <njy3006@gmail.com>
2025-01-26 00:13:22 +0900

    Title: 체크판위에서 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250126/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,15 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+count = 0
+
+color = grid[0][0]
+
+for i in range(R-2):
+    for j in range(C-2):
+        for k in range(i+1, R-1):
+            for h in range(j+1, C-1):
+                if grid[i][j] != color and grid[k][h] == color:
+                    count += 1
+
+print(count)
