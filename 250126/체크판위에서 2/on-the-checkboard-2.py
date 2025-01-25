364a5c551dd8b9ad57227f9d35589f757da37116
Jun-Young Noh <njy3006@gmail.com>
2025-01-26 00:18:39 +0900

    Title: 체크판위에서 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250126/체크판위에서 2/on-the-checkboard-2.py
+++ 250126/체크판위에서 2/on-the-checkboard-2.py
@@ -5,11 +5,11 @@ count = 0
 
 color = grid[0][0]
 
-for i in range(R-2):
-    for j in range(C-2):
+for i in range(1, R-2):
+    for j in range(1, C-2):
         for k in range(i+1, R-1):
             for h in range(j+1, C-1):
-                if grid[i][j] != color and grid[k][h] == color and i < k and j < h and grid[R-1][C-1] != color:
+                if grid[i][j] != color and grid[k][h] == color and grid[R-1][C-1] != color:
                     count += 1
 
 print(count)
