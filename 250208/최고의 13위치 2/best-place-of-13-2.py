b6d32073bbd3082766b9538cfe108c8331176158
Jun-Young Noh <njy3006@gmail.com>
2025-02-08 21:06:55 +0900

    Title: 최고의 13위치 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250208/최고의 13위치 2/best-place-of-13-2.py
+++ 250208/최고의 13위치 2/best-place-of-13-2.py
@@ -10,7 +10,7 @@ for i in range(n):
     for j in range(n):
         for k in range(n):
             if i == k:
-                for h in range(j+2, n):
+                for h in range(j+3, n):
                     if in_range(i, j+2) and in_range(k, h+2):
                         sum_value = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][h] + arr[k][h+1] + arr[k][h+2]
                         max_sum = max(max_sum, sum_value)
