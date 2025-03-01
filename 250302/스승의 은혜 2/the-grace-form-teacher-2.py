1abe2923858500af2059e2a07150dd20b9b221e7
Jun-Young Noh <njy3006@gmail.com>
2025-03-02 02:45:16 +0900

    Title: 스승의 은혜 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250302/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 250302/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -4,16 +4,21 @@ p = [int(input()) for _ in range(n)]
 maxCount = 0
 
 for i in range(n):
-    count = 0
     sumValue = p[i] // 2
+    if sumValue <= b:
+        count = 1
+    else:
+        count = 0
     for j in range(n):
         if i == j:
             continue
+
+        sumValue += p[j]
         if sumValue <= b:
             count += 1
         else:
             break
-        sumValue += p[j]
+        
     maxCount = max(maxCount, count)
 
 print(maxCount)
