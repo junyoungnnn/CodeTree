b4a9f89142e2e35151c5691cb3ae98967ceaef36
Jun-Young Noh <njy3006@gmail.com>
2025-03-05 01:09:34 +0900

    Title: 스승의 은혜 3, Time: 70ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250305/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250305/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,6 +1,7 @@
 n, b = map(int, input().split())
 s = [list(map(int, input().split())) for _ in range(n)]
-s.sort(key = lambda x: (x[0]//2 + x[1]))
+s.sort(key = lambda x: [(x[0]//2 + x[1]), (x[0] + x[1])])
+
 maxCount = 0
 
 for i in range(n):
@@ -12,9 +13,11 @@ for i in range(n):
             continue
 
         count += 1
+        value += s[j][0] + s[j][1]
         if value >= b:
             break
-        value += s[j][0] + s[j][1]
+    else:
+        count += 1
 
     maxCount = max(maxCount, count)
 
