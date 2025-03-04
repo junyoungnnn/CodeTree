f51341840a14fa9211fbaa916883f32b99999384
Jun-Young Noh <njy3006@gmail.com>
2025-03-05 02:20:17 +0900

    Title: 스승의 은혜 3, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250305/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,17 +1,21 @@
 n, b = map(int, input().split())
 s = [list(map(int, input().split())) for _ in range(n)]
-s.sort(key = lambda x: [(x[0]//2 + x[1]), (x[0] + x[1])])
-
+s.sort(key = lambda x: [(x[0]//2 + x[1])])
+t = s[:]
+t.sort(key = lambda x: [(x[0] + x[1])])
 maxCount = 0
 
 for i in range(n):
-    count = 0
-    value = 0
+    count = 1
+    value = s[i][0] // 2 + s[i][1]
+    left = s[i][0]
+    right = s[i][1]
+
     for j in range(n):
-        if i == j:
-            value += s[i][0] // 2 + s[i][1]
+        if left == t[j][0] and right == t[j][1]:
+            continue
         else:
-            value += s[j][0] + s[j][1]
+            value += t[j][0] + t[j][1]
 
         if value <= b:
             count += 1
