b4e0d1e59ccf1bff4d9b4c80eb1def35eb1214db
Jun-Young Noh <njy3006@gmail.com>
2025-03-08 02:46:37 +0900

    Title: 등차수열, Time: 72ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 250308/등차수열/arithmetic-sequence.py
+++ 250308/등차수열/arithmetic-sequence.py
@@ -2,15 +2,17 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-count = 0
+maxCount = 0
 
 for i in range(1, 51):
+    count = 0
     for j in range(n):
         for h in range(j+1, n):
             if arr[j] == arr[h]:
                 continue
             if abs(arr[j]-i) == abs(arr[h]-i):
                 count += 1
+    maxCount = max(maxCount, count)
 
-print(count)
+print(maxCount)
 
