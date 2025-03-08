0f0944cdf19f2290f841a555cd297b174a8253c8
Jun-Young Noh <njy3006@gmail.com>
2025-03-09 03:06:25 +0900

    Title: 야바위, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/야바위/ya-rock.py
+++ 250309/야바위/ya-rock.py
@@ -3,8 +3,7 @@ n = int(input())
 A = []
 B = []
 C = []
-arr = [0] * 4
-index = 0
+
 maxCount = 0
 
 for i in range(n):
@@ -13,19 +12,17 @@ for i in range(n):
     B.append(b)
     C.append(c)
 
-for i in range(1, n+1):
+for i in range(1, 4):
+    arr = [0] * 4
     arr[i] = 1
     count = 0
     for j in range(n):
         arr[A[j]], arr[B[j]] = arr[B[j]], arr[A[j]]
         if arr[C[j]] == 1:
             count += 1
-    if maxCount < count:
-        maxCount = count
-        index = i
+    maxCount = max(maxCount, count)
 
-print(index)
-
+print(maxCount)
 
 
 
