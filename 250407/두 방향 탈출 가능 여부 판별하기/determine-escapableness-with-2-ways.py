76761d363164ac10f263ca4a1e4bdeb486fd75d2
Jun-Young Noh <njy3006@gmail.com>
2025-04-07 01:07:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250407/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,33 @@
+N, M = map(int, input().split())
+gr = [list(map(int, input().split())) for _ in range(M)]
+
+visited = [[0 for i in range(N+1)] for _ in range(M+1)]
+
+row = [1, 0]
+col = [0, 1]
+
+answer = 0
+
+def dfs(r, c):
+    global answer
+    
+    if r == N-1 and c == M-1:
+        answer = 1
+        return
+
+    for i in range(2):
+        nr = r + row[i]
+        nc = c + col[i]
+
+        if nr < 0 or nr >= N or nc < 0 or nc >= M:
+            continue
+        if visited[nr][nc] == 1:
+            continue
+        if gr[nr][nc] == 1:
+            visited[nr][nc] = 1
+            dfs(nr, nc)
+
+visited[0][0] = 1
+dfs(0, 0)
+
+print(answer)
