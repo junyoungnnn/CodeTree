4c0b913098143a936375dad5763ced60722a4a3f
Jun-Young Noh <njy3006@gmail.com>
2025-04-07 00:29:54 +0900

    Title: 그래프 탐색, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250407/그래프 탐색/graph-traversal.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+
+gr = [[] for _ in range(N+1)]
+
+visited = [0 for _ in range(N+1)]
+print(gr)
+cnt = 0
+
+def dfs(vertex):
+    global cnt
+
+    for curr in gr[vertex]:
+
+        if not visited[curr]:
+            visited[curr] = 1
+            cnt += 1
+            dfs(curr)
+
+for i in range(M):
+    v1, v2 = map(int, input().split())
+
+    gr[v1].append(v2)
+    gr[v2].append(v1)
+
+visited[1] = 1
+dfs(1)
+
+print(cnt)
